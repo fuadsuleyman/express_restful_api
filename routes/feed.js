a23e348f8853945c144e191af202fc68a0d1eef9
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-07-09 10:48:33 +0400

    add express-validator server side validation to creatPost

--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed')
 
@@ -9,6 +10,13 @@ router.get('/posts', feedController.getPosts);
 
 
 // POST api/post
-router.post('/post', feedController.createPost);
+router.post('/post', [
+    body('title')
+        .trim()
+        .isLength({ min:5 }),
+    body('content')
+        .trim()
+        .isLength({ min:5 })
+], feedController.createPost);
 
 module.exports = router;

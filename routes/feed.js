fae37f20fe1a8ab86195bbf5a718c59c8ae22aaa
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-07-23 21:24:06 +0400

    created middleware to use jwt and use this in every routes

--- routes/feed.js
+++ routes/feed.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const { body } = require('express-validator');
 
-const feedController = require('../controllers/feed')
+const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // GET api/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 
 // POST api/post
-router.post('/post', [
+router.post('/post', isAuth, [
     body('title')
         .trim()
         .isLength({ min:5 }),
@@ -20,10 +21,10 @@ router.post('/post', [
 ], feedController.createPost);
 
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
 
 
-router.put('/post/:postId', [
+router.put('/post/:postId', isAuth, [
     body('title')
         .trim()
         .isLength({ min:5 }),
@@ -33,6 +34,6 @@ router.put('/post/:postId', [
 ], feedController.updatePost);
 
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;

bc6cd112bdfbc3023f4237935e60868f32b7e5a6
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-07-23 12:37:31 +0400

    add bcrypt.js, singup is ready

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { body } = require('express-validator');
 
+const User = require('../models/user');
+
 const authController = require('../controllers/auth')
 
 const router = express.Router();

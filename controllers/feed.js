4c20151212b7c1443f15ab08b7e6d0ef8e18fac8
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-07-23 23:09:18 +0400

    connecting Posts and User models

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const { validationResult } = require('express-validator');
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -47,18 +48,30 @@ exports.createPost = (req, res, next) => {
     const imageUrl = req.file.path;
     const content = req.body.content;
     const title = req.body.title;
+    let creator;
     const post = new Post({
         title: title,
         content: content,
         imageUrl: imageUrl,
-        creator: {name: 'FuadS'}
+        creator: req.userId
     });
     post.save()
         .then(result => {
-            console.log(result)
+            return User.findById(req.userId);
+        })
+        .then(user => {
+            creator = user;
+            user.posts.push(post);
+            return user.save();
+        })
+        .then(result => {
             res.status(201).json({
                 message: 'Post created succussfully!',
-                post: result
+                post: post,
+                creator: {
+                    _id: creator._id,
+                    name: creator.name
+                }
             })
         })
         .catch(err => {

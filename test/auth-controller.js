59d5a40a75e5ce0e908428126d6420ca2cef5552
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-07-31 11:53:34 +0400

    using mocha hooks before and after, but we have beforeEach and afterEach thats I dont use here

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -4,7 +4,26 @@ const expect = require('chai').expect;
 const User = require('../models/user');
 const mongoose = require('mongoose');
 
-describe('Auth Controller - Login', function(){
+describe('Auth Controller', function(){
+    before(function(done){
+        mongoose.connect(
+            'mongodb+srv://fuads:wLoyopyig03cXY9a@cluster0.oepjk.mongodb.net/test-feedDB?retryWrites=true&w=majority',
+            {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}
+            )
+            .then(result => {
+                const user = new User({
+                    email: 'test5@test.com',
+                    password: 'tester',
+                    name:'Test',
+                    posts: [],
+                    _id: '5c0f66bd979af55031b3472a'
+                })
+                return user.save()
+              })
+              .then(() => {
+                  done(); 
+              })
+    })
     it('it should throw an error with code 500 if accessing the database fails', function(done){
         sinon.stub(User, 'findOne');
         User.findOne.throws();
@@ -29,21 +48,8 @@ describe('Auth Controller - Login', function(){
     })
 
     it('should send a rsponse with a valid user status for an existing user', function(done){
-        mongoose.connect(
-            'mongodb+srv://fuads:wLoyopyig03cXY9a@cluster0.oepjk.mongodb.net/test-feedDB?retryWrites=true&w=majority',
-            {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}
-            )
-            .then(result => {
-                const user = new User({
-                    email: 'test5@test.com',
-                    password: 'tester',
-                    name:'Test',
-                    posts: [],
-                    _id: '5c0f66bd979af55031b3472a'
-                })
-                return user.save()
-              })
-              .then(() =>{
+        
+    
                 const req = {userId: '5c0f66bd979af55031b3472a'}
                 const res = {
                     statusCode: 500,
@@ -59,15 +65,17 @@ describe('Auth Controller - Login', function(){
                 authController.getStatus(req, res, () => {}).then(()=>{
                     expect(res.statusCode).to.be.equal(200);
                     expect(res.userStatus).to.be.equal('I am new!');
-                    // below code delete all users
-                    User.deleteMany({}).then(() => {
-                        return mongoose.disconnect()
-                    })
-                    .then(() => {
-                        done();
-                    });
+                    done()
                 })
-              })
-              .catch(err => console.log(err));
+  
     })
+    after(function(done){
+    // below code delete all users
+        User.deleteMany({}).then(() => {
+           return mongoose.disconnect()
+        })
+        .then(() => {
+            done();
+        });
+        })
 })
